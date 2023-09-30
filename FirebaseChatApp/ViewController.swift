28276f39474beb79147911ff2eb1bcf55086e90b
Mohd Wasif Raza <wasraz86@gmail.com>
2023-09-30 11:03:57 +0530

    Initial Commit

---
+++ FirebaseChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by Mohd Wasif Raza on 30/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
